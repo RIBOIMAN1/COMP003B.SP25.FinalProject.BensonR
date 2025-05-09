04c14222fd3fe30fca838626e4da5cc7d30feace
RIBOIMAN1 <rileybenson@my.whccd.edu>
2025-05-09 10:18:43 -0700

    Add project files.

---
+++ COMP003B.SP25.FinalProject.BensonR/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace COMP003B.SP25.FinalProject.BensonR.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
