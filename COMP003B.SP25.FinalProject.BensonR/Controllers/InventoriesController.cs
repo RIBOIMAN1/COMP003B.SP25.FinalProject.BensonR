ccff20d6b1228133f7bf395d2805c9b52fefc2fa
RIBOIMAN1 <rileybenson@my.whccd.edu>
2025-05-11 06:40:21 -0700

    Used scaffolding to add views for each of the models in my project and added controllers for each of these models as well.

---
+++ COMP003B.SP25.FinalProject.BensonR/Controllers/InventoriesController.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using COMP003B.SP25.FinalProject.BensonR.Data;
+using COMP003B.SP25.FinalProject.BensonR.Models;
+
+namespace COMP003B.SP25.FinalProject.BensonR.Controllers
+{
+    public class InventoriesController : Controller
+    {
+        private readonly ComputerContext _context;
+
+        public InventoriesController(ComputerContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Inventories
+        public async Task<IActionResult> Index()
+        {
+            var computerContext = _context.Inventories.Include(i => i.Technician);
+            return View(await computerContext.ToListAsync());
+        }
+
+        // GET: Inventories/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var inventory = await _context.Inventories
+                .Include(i => i.Technician)
+                .FirstOrDefaultAsync(m => m.InventoryId == id);
+            if (inventory == null)
+            {
+                return NotFound();
+            }
+
+            return View(inventory);
+        }
+
+        // GET: Inventories/Create
+        public IActionResult Create()
+        {
+            ViewData["TechnicianId"] = new SelectList(_context.Technicians, "TechnicianId", "Email");
+            return View();
+        }
+
+        // POST: Inventories/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("InventoryId,TechnicianId,Name,CompType")] Inventory inventory)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(inventory);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["TechnicianId"] = new SelectList(_context.Technicians, "TechnicianId", "Email", inventory.TechnicianId);
+            return View(inventory);
+        }
+
+        // GET: Inventories/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var inventory = await _context.Inventories.FindAsync(id);
+            if (inventory == null)
+            {
+                return NotFound();
+            }
+            ViewData["TechnicianId"] = new SelectList(_context.Technicians, "TechnicianId", "Email", inventory.TechnicianId);
+            return View(inventory);
+        }
+
+        // POST: Inventories/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("InventoryId,TechnicianId,Name,CompType")] Inventory inventory)
+        {
+            if (id != inventory.InventoryId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(inventory);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!InventoryExists(inventory.InventoryId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["TechnicianId"] = new SelectList(_context.Technicians, "TechnicianId", "Email", inventory.TechnicianId);
+            return View(inventory);
+        }
+
+        // GET: Inventories/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var inventory = await _context.Inventories
+                .Include(i => i.Technician)
+                .FirstOrDefaultAsync(m => m.InventoryId == id);
+            if (inventory == null)
+            {
+                return NotFound();
+            }
+
+            return View(inventory);
+        }
+
+        // POST: Inventories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var inventory = await _context.Inventories.FindAsync(id);
+            if (inventory != null)
+            {
+                _context.Inventories.Remove(inventory);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool InventoryExists(int id)
+        {
+            return _context.Inventories.Any(e => e.InventoryId == id);
+        }
+    }
+}
---
+++ COMP003B.SP25.FinalProject.BensonR/Controllers/RepairTicketsController.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using COMP003B.SP25.FinalProject.BensonR.Data;
+using COMP003B.SP25.FinalProject.BensonR.Models;
+
+namespace COMP003B.SP25.FinalProject.BensonR.Controllers
+{
+    public class RepairTicketsController : Controller
+    {
+        private readonly ComputerContext _context;
+
+        public RepairTicketsController(ComputerContext context)
+        {
+            _context = context;
+        }
+
+        // GET: RepairTickets
+        public async Task<IActionResult> Index()
+        {
+            var computerContext = _context.RepairTickets.Include(r => r.Customer).Include(r => r.Inventory).Include(r => r.Invoice).Include(r => r.Technician);
+            return View(await computerContext.ToListAsync());
+        }
+
+        // GET: RepairTickets/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var repairTicket = await _context.RepairTickets
+                .Include(r => r.Customer)
+                .Include(r => r.Inventory)
+                .Include(r => r.Invoice)
+                .Include(r => r.Technician)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (repairTicket == null)
+            {
+                return NotFound();
+            }
+
+            return View(repairTicket);
+        }
+
+        // GET: RepairTickets/Create
+        public IActionResult Create()
+        {
+            ViewData["CustomerId"] = new SelectList(_context.Customers, "CustomerId", "Email");
+            ViewData["InventoryId"] = new SelectList(_context.Inventories, "InventoryId", "CompType");
+            ViewData["InvoiceId"] = new SelectList(_context.Invoices, "InvoiceId", "PayStatus");
+            ViewData["TechnicianId"] = new SelectList(_context.Technicians, "TechnicianId", "Email");
+            return View();
+        }
+
+        // POST: RepairTickets/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,CustomerId,TechnicianId,InventoryId,InvoiceId")] RepairTicket repairTicket)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(repairTicket);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CustomerId"] = new SelectList(_context.Customers, "CustomerId", "Email", repairTicket.CustomerId);
+            ViewData["InventoryId"] = new SelectList(_context.Inventories, "InventoryId", "CompType", repairTicket.InventoryId);
+            ViewData["InvoiceId"] = new SelectList(_context.Invoices, "InvoiceId", "PayStatus", repairTicket.InvoiceId);
+            ViewData["TechnicianId"] = new SelectList(_context.Technicians, "TechnicianId", "Email", repairTicket.TechnicianId);
+            return View(repairTicket);
+        }
+
+        // GET: RepairTickets/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var repairTicket = await _context.RepairTickets.FindAsync(id);
+            if (repairTicket == null)
+            {
+                return NotFound();
+            }
+            ViewData["CustomerId"] = new SelectList(_context.Customers, "CustomerId", "Email", repairTicket.CustomerId);
+            ViewData["InventoryId"] = new SelectList(_context.Inventories, "InventoryId", "CompType", repairTicket.InventoryId);
+            ViewData["InvoiceId"] = new SelectList(_context.Invoices, "InvoiceId", "PayStatus", repairTicket.InvoiceId);
+            ViewData["TechnicianId"] = new SelectList(_context.Technicians, "TechnicianId", "Email", repairTicket.TechnicianId);
+            return View(repairTicket);
+        }
+
+        // POST: RepairTickets/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,CustomerId,TechnicianId,InventoryId,InvoiceId")] RepairTicket repairTicket)
+        {
+            if (id != repairTicket.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(repairTicket);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!RepairTicketExists(repairTicket.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CustomerId"] = new SelectList(_context.Customers, "CustomerId", "Email", repairTicket.CustomerId);
+            ViewData["InventoryId"] = new SelectList(_context.Inventories, "InventoryId", "CompType", repairTicket.InventoryId);
+            ViewData["InvoiceId"] = new SelectList(_context.Invoices, "InvoiceId", "PayStatus", repairTicket.InvoiceId);
+            ViewData["TechnicianId"] = new SelectList(_context.Technicians, "TechnicianId", "Email", repairTicket.TechnicianId);
+            return View(repairTicket);
+        }
+
+        // GET: RepairTickets/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var repairTicket = await _context.RepairTickets
+                .Include(r => r.Customer)
+                .Include(r => r.Inventory)
+                .Include(r => r.Invoice)
+                .Include(r => r.Technician)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (repairTicket == null)
+            {
+                return NotFound();
+            }
+
+            return View(repairTicket);
+        }
+
+        // POST: RepairTickets/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var repairTicket = await _context.RepairTickets.FindAsync(id);
+            if (repairTicket != null)
+            {
+                _context.RepairTickets.Remove(repairTicket);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool RepairTicketExists(int id)
+        {
+            return _context.RepairTickets.Any(e => e.Id == id);
+        }
+    }
+}
