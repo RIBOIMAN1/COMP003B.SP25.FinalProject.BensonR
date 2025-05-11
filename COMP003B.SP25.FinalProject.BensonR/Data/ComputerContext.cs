b360637e788bb1687c42e8fbdf158c9cbe60e317
RIBOIMAN1 <rileybenson@my.whccd.edu>
2025-05-10 17:38:10 -0700

    Added DbContext code for each of the five models in my project: "Customer," "Technician," "RepairTicket," "Inventory," and "Invoice" in the file "ComputerContext.cs."

---
+++ COMP003B.SP25.FinalProject.BensonR/Data/ComputerContext.cs
@@ -0,0 +1,18 @@
+﻿using COMP003B.SP25.FinalProject.BensonR.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace COMP003B.SP25.FinalProject.BensonR.Data
+{
+	public class ComputerContext : DbContext
+	{
+		public ComputerContext(DbContextOptions<ComputerContext> options) : base(options)
+		{
+		}
+
+		public DbSet<Customer> Customers { get; set; }
+		public DbSet<Technician> Technicians { get; set; }
+		public DbSet<RepairTicket> RepairTickets { get; set; }
+		public DbSet<Inventory> Inventories { get; set; }
+		public DbSet<Invoice> Invoices { get; set; }
+	}
+}
--- COMP003B.SP25.FinalProject.BensonR/Data/ControllerContext.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace COMP003B.SP25.FinalProject.BensonR.Data
-{
-	public class ControllerContext
-	{
-	}
-}
